203937af2be73cc588399bc8b0be8582ac64eb19
Tadeo Joaquín Cárdenas Soto <tadeojoacarso@gmail.com>
2025-06-18 17:34:32 -0500

    Update a68g-utf8.bat

--- scripts/a68g-utf8.bat
+++ scripts/a68g-utf8.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001 > nul
-"LETRA:\Users\tu_usuario\LUGAR\algol68g-3.5.14.win32\algol68g-3.5.14\bin\a68g.exe" %*
+"LETTER:\Users\your_username\LOCATION\algol68g-3.5.14.win32\algol68g-3.5.14\bin\a68g.exe" %*
